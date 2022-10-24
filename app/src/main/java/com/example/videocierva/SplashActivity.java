0e80a4cc640e90d832ec75c37ba1f264bdde29af
DESKTOP\Diego <4261132@alu.murciaeduca.es>
2022-10-24 13:51:46 +0200

    Cambio de Logo y Splash con animación

---
+++ app/src/main/java/com/example/videocierva/SplashActivity.java
@@ -0,0 +1,36 @@
+package com.example.videocierva;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.splash_activity);
+
+        Animation animacion = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_arriba);
+
+        ImageView logo = findViewById(R.id.imgLogo);
+
+        logo.setAnimation(animacion);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 4000);
+
+    }
+}
