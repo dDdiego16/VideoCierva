1d0e8e30f7608b362821cf1e6da3ff37de7e5025
DESKTOP\Diego <4261132@alu.murciaeduca.es>
2022-10-25 19:28:56 +0200

    Login a falta de algún retoque.

--- app/src/main/java/com/example/videocierva/LoginActivity.java
+++ app/src/main/java/com/example/videocierva/LoginActivity.java
@@ -1,13 +1,59 @@
 package com.example.videocierva;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText correo, passwd;
+    Button buttonLog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        correo = findViewById(R.id.emailLog);
+        passwd = findViewById(R.id.passwdLog);
+        buttonLog = findViewById(R.id.butLog);
+
+        buttonLog.setOnClickListener(view -> {
+            if(!validarCampos()) {
+                Toast.makeText(view.getContext(), "Correcto", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(view.getContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+    public boolean validarCampos() {
+
+        boolean error = false;
+
+        String correoLog = correo.getText().toString();
+        String passwdLog = passwd.getText().toString();
+
+        if(correoLog.isEmpty()) {
+            correo.setError("Inserte el correo");
+            error = true;
+        }
+        if(passwdLog.isEmpty()) {
+            passwd.setError("Inserte la contraseña.");
+            error = true;
+        }
+        if(!correoLog.isEmpty() && !correoLog.contains("@")) {
+            correo.setError("Inserte un correo válido.");
+            error = true;
+        }
+
+        return error;
+
+    }
+
 }
