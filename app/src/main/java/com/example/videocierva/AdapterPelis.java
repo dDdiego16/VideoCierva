0aa27f50fa6c44ccbfb9176b7c4cf2c74ba9cc51
DESKTOP\Diego <4261132@alu.murciaeduca.es>
2022-12-09 02:29:35 +0100

    Buscador Boton Peliculas y Arreglo de Warning en Layouts

--- app/src/main/java/com/example/videocierva/AdapterPelis.java
+++ app/src/main/java/com/example/videocierva/AdapterPelis.java
@@ -7,31 +7,32 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.bumptech.glide.Glide;
-
+import java.util.ArrayList;
 import java.util.List;
 
 public class AdapterPelis extends RecyclerView.Adapter<AdapterPelis.ViewHolder> {
 
     private Context mCtx;
     private List<Pelicula> pelisList;
+    private ArrayList<Pelicula> pelisFiltradas;
 
     public AdapterPelis(Context mCtx, List<Pelicula> pelisList) {
         this.mCtx = mCtx;
         this.pelisList = pelisList;
+        pelisFiltradas = new ArrayList<>();
+        pelisFiltradas.addAll(pelisList);
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        TextView textTitulo, textAño, textCateg, textDisp;
+        TextView textTitulo, textAge, textCateg, textDisp;
         ImageView fotoPeli;
 
         public ViewHolder(View view) {
             super(view);
             textTitulo = (TextView) view.findViewById(R.id.txtNombrePeli);
-            textAño = (TextView) view.findViewById(R.id.txtAñoPeli);
+            textAge = (TextView) view.findViewById(R.id.txtAñoPeli);
             textCateg = (TextView) view.findViewById(R.id.txtCategoriaPeli);
             textDisp = (TextView) view.findViewById(R.id.txtDisponiblePeli);
             fotoPeli = (ImageView) view.findViewById(R.id.imagenPeli);
@@ -52,7 +53,7 @@ public class AdapterPelis extends RecyclerView.Adapter<AdapterPelis.ViewHolder> 
                 .load(peli.getFoto())
                 .into(viewHolder.fotoPeli);
         viewHolder.textTitulo.setText(peli.getTitulo());
-        viewHolder.textAño.setText(String.valueOf(peli.getAño()));
+        viewHolder.textAge.setText(String.valueOf(peli.getAño()));
         viewHolder.textCateg.setText(peli.getCategoria());
         if(peli.getDisponible().equals("0")) {
             viewHolder.textDisp.setText("Disponible");
@@ -61,6 +62,22 @@ public class AdapterPelis extends RecyclerView.Adapter<AdapterPelis.ViewHolder> 
             viewHolder.textDisp.setText("No Disponible");
             viewHolder.textDisp.setTextColor(Color.parseColor("#FF0000"));
         }
+    }
+
+    public void filtrado(String txtBuscar) {
+
+        if(txtBuscar.length() == 0) {
+            pelisList.clear();
+            pelisList.addAll(pelisFiltradas);
+        } else {
+            pelisList.clear();
+            for (Pelicula c: pelisFiltradas) {
+                if(c.getTitulo().toLowerCase().contains(txtBuscar.toLowerCase())){
+                    pelisList.add(c);
+                }
+            }
+        }
+        notifyDataSetChanged();
     }
 
     // Return the size of your dataset (invoked by the layout manager)
--- app/src/main/java/com/example/videocierva/PelisActivity.java
+++ app/src/main/java/com/example/videocierva/PelisActivity.java
@@ -3,81 +3,81 @@ package com.example.videocierva;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.content.Intent;
 import android.os.Bundle;
+import android.widget.SearchView;
 import android.widget.Toast;
-
 import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.util.ArrayList;
 import java.util.List;
 
-public class PelisActivity extends AppCompatActivity {
+public class PelisActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
 
-
+    SearchView txtBuscar;
     private static final String URL_pelis = "http://192.168.1.83/dam/pelis.php";
     List<Pelicula> peliculaList;
     RecyclerView recyclerView;
+    AdapterPelis mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pelis);
-        
+
+        txtBuscar = findViewById(R.id.buscadorPelis);
         recyclerView = findViewById(R.id.recyclerViewPelis);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         
         peliculaList = new ArrayList<>();
-        
         loadpelis();
+
+        txtBuscar.setOnQueryTextListener(this);
         
     }
 
     private void loadpelis() {
 
         StringRequest stringRequest = new StringRequest(Request.Method.GET, URL_pelis,
-                new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                try {
-                    JSONArray array = new JSONArray(response);
+                response -> {
+                    try {
+                        JSONArray array = new JSONArray(response);
 
-                    for (int i = 0; i < array.length(); i++) {
+                        for (int i = 0; i < array.length(); i++) {
 
-                        JSONObject peli = array.getJSONObject(i);
+                            JSONObject peli = array.getJSONObject(i);
 
-                        peliculaList.add(new Pelicula(
-                                peli.getString("name_peli"),
-                                peli.getInt("age_peli"),
-                                peli.getString("category_peli"),
-                                peli.getString("image_peli"),
-                                peli.getString("disponible_peli")
-                        ));
+                            peliculaList.add(new Pelicula(
+                                    peli.getString("name_peli"),
+                                    peli.getInt("age_peli"),
+                                    peli.getString("category_peli"),
+                                    peli.getString("image_peli"),
+                                    peli.getString("disponible_peli")
+                            ));
+                        }
+
+                        mAdapter = new AdapterPelis(PelisActivity.this, peliculaList);
+                        recyclerView.setAdapter(mAdapter);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
                     }
-
-                    AdapterPelis mAdapter = new AdapterPelis(PelisActivity.this, peliculaList);
-                    recyclerView.setAdapter(mAdapter);
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(PelisActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
-            }
-        });
+                }, error -> Toast.makeText(PelisActivity.this, error.toString(), Toast.LENGTH_SHORT).show());
         Volley.newRequestQueue(this).add(stringRequest);
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String s) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String s) {
+        mAdapter.filtrado(s);
+        return false;
     }
 }
